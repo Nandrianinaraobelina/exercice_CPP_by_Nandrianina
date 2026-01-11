ef00b63d8aa48b79d9c893be67d74ba97d9f6965
Nandrianina <nandrianina@gmail.com>
2026-01-11 14:39:25 +0300

    Exercice 5 , cible ..plus Grand valeur

---
+++ exercice_5/exercice_5.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+/*
+    Ecrire un programme qui permet d'afficher le plus Grand de trois
+    entiers saisis au clavier
+*/
+int main()
+ {
+    cout << "Veuillez entrer les 3 valeurs \n";
+    int A ,B ,C , plusGrand ;
+    cout << "Valeur de A est : "; cin >> A;
+    cout << "Valeur de B est : "; cin >> B;
+    cout << "Valeur de C est : "; cin >> C;
+ 
+    plusGrand = A;
+    if(B >= plusGrand)
+    plusGrand = B;
+    if(C >= plusGrand)
+    plusGrand = C;
+    
+    cout << "La valeur plus Grand est: "<<plusGrand<<endl;
+    return 0;
+
+ }
