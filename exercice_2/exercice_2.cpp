11fb81428d792f32592dc54e29868c7f9e1a0a82
Nandrianina <nandrianina@gmail.com>
2026-01-11 10:17:07 +0300

    Exercice 2 VITA Ooooooo

--- exercice_2/exercice_2.cpp
+++ exercice_2/exercice_2.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 /*
 
-ECRIRE UN PROGRAMME PERMETTANT D
+ECRIRE UN PROGRAMME PERMETTANT De saisir deux nombre et
+d'afficher leur produit
 
 */
 
 int main()
 {
-    cout << "Bonjour";
+    cout << "Veuillez saisir 2 nombre et afficher leur Produit \n";
+    int a , b;
+    cout << "NOMBRE a est" << endl;cin >> a; 
+    cout << "NOMBRE b est" << endl;cin >> b;
+
+    cout << "le nombre a =  "  <<  a  << " et le nombre b  =  " <<  b  << endl;
     return 0;
 } 
  
