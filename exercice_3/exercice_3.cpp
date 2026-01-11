bbf79ae527261c79d7e9da6919707e99f948a0d4
Nandrianina <nandrianina@gmail.com>
2026-01-11 11:02:28 +0300

    Exercice 3, vita ...

--- exercice_3/exercice_3.cpp
+++ exercice_3/exercice_3.cpp
@@ -2,13 +2,23 @@
 using namespace std;
 /*
 
-ECRIRE UN PROGRAMME PERMETTANT D'AFFICHER LE MESSAGE "BONJOUR"
+ECRIRE UN PROGRAMME qui permet d'echanger le contenu
+ de deux entiers A et B saisis par ISER et afficher ces entiers après l'échange
 
 */
 
 int main()
 {
-    cout << "Bonjour";
+    cout << "Entrer les deux valeurs \n";
+    int A,B,C;
+    cout <<"Valeur de a = ";cin >> A;
+    cout <<"Valeir de b = ";cin >> B;
+    cout <<"----------------------\n";
+    cout << "changement de valeur par l'utilisateur: \n";
+    C = A;
+    A = B;
+    B = C;
+    cout << "donc la valeur de A = "<< A  << " et la valeur de B = " << B << endl;
     return 0;
 } 
  
