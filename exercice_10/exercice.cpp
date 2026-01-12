ce9b6977f121db94064f539b11733aeb823ada86
Nandrianina <nandrianina@gmail.com>
2026-01-12 08:30:34 +0300

    OOP

---
+++ exercice_10/exercice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+
+    Une boutique Propose à ces clients,une reductions de
+    15% pour le montants d'achats superieurs à 200dh.
+
+    Ecrire un programme permettant de saisir le prix TOTAL HT et de calculer
+    le montants TTC en prenant en compte la reductions et la TVA = 20%
+
+*/
+
+int main(){
+
+}
